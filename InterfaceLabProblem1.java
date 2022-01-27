3f1f08d0149fb6d39811f07a3f24763349ce332e
nairvishnumail <76607707+nairvishnumail@users.noreply.github.com>
2022-01-27 09:58:02 -0800

    Update InterfaceLabProblem1.java

--- InterfaceLabProblem1.java
+++ InterfaceLabProblem1.java
@@ -1,9 +1,18 @@
+/*
+1.      
+Create a class that implements:
+
+public interface MSJstuff {
+  public void displaylhospitalRule();
+  public boolean isLiu( String x );
+  public int round( double a ) //round .5 up and below down  
+}
+*/
 
 public class InterfaceLabProblem1 implements MSJstuff{
 	public void displaylhospitalRule() {
 		System.out.println("It's a math rule");
 	}
-
 	public boolean isLiu (String x) {
 		if (x.equals("Liu")) {
 			return true;
