adebae86822e5653dd807d8eb22037a2c1b41afe
nairvishnumail <76607707+nairvishnumail@users.noreply.github.com>
2022-01-27 09:55:21 -0800

    Update InterfaceLabProblem3.java

--- InterfaceLabProblem3.java
+++ InterfaceLabProblem3.java
@@ -1,3 +1,20 @@
+/*
+Create an interface class called First with one method called yelena.
+
+Create an ABSTRACT class called Second that inherits it.
+
+DO NOT Implement yelena in Second
+
+Create a class called Third that inherits from Second
+
+Implement yelena in Third
+
+Run yelena in Main from a variable of type
+Third to make sure that yelena works
+
+Turn in on google classroom Interface3
+*/
+
 
 public class InterfaceLabProblem3 {
 
